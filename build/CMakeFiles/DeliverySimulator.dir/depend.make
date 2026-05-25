fb38da37114c52d89fdea52b0819e2184d967dcb
hibamoudoud2006 <hiba.moudoud.24@edu.uiz.ac.ma>
2026-05-26 00:38:27 +0100

    Initial project

--- build/CMakeFiles/DeliverySimulator.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for DeliverySimulator.
-# This may be replaced when dependencies are built.
--- build/CMakeFiles/DeliverySimulator.dir/src/Dimensions.cpp.obj.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/DeliverySimulator.dir/src/Dimensions.cpp.obj: \
- C:\Users\HP\OneDrive\Bureau\cpp_project\src\Dimensions.cpp \
- C:/Users/HP/OneDrive/Bureau/cpp_project/include/Dimensions.h
